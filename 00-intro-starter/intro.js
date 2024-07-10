0bb1510ec8ed3d2b7f9df6aebd4585ff24f186bf
GabrielDisch <gabriel@whiteblazes.com>
2024-07-10 15:23:33 -0400

    Added stuff

--- 00-intro-starter/intro.js
+++ 00-intro-starter/intro.js
@@ -3,26 +3,30 @@
 //  1. Give every variable/const smallest scope
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
-const colorInput = document.getElementById("colorID");
-const buttonInput = document.getElementById("button1");
-const textBox = document.getElementById("demo");
+
+const colorField = document.getElementById("color-field");
+const fontSizeField = document.getElementById("font-size-field");
+const clickButton = document.getElementById("click-button");
+const text = document.getElementById("text");
+const checkbox = document.getElementById("checkbox");
 const body = document.body;
-const checkBox = document.getElementById("checkbox");
-
-function checkBoxPress()
-{
-    let checked = checkBox.checked;
-    console.log('Checkbox is ${checked}');
-
-    if (checked)
-    {
-        body.style.borderStyle = "solid";
-    }
-}
+// let (or var) to declare variables
 
 function buttonPress() {
-text.innerHTML = "test test test";
+    text.innerHTML = "Wow! You pressed the button!";
 
-body.style.backgroundColor = colorInput.value;
+    body.style.backgroundColor = colorField.value;
+    text.style.fontSize = `${fontSizeField.value}px`; // NOTE: the [`] key is above the [Tab] key
+    let fail = '${fontSizeField.value}px'; // Apostrophe will not work
 }
-``
+
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`The checkbox has this state: ${checked}`);
+
+    if (checked) {
+        body.style.outlineStyle = "solid";
+    } else {
+        body.style.outlineStyle = "none";
+    }
+}
