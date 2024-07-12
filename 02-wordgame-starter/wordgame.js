90f01915c2da689880e5655db26087731b9c1194
GabrielDisch <gabriel@whiteblazes.com>
2024-07-12 09:43:54 -0400

    stuff

--- 02-wordgame-starter/wordgame.js
+++ 02-wordgame-starter/wordgame.js
@@ -1,4 +1,5 @@
 let json;
+const body = document.body;
 
 // Returns a random integer between min and max
 //   [min, min+1, min+2, ... , max-1, max]
@@ -17,23 +18,64 @@ function loadGame() {
             // Split the text by lines to get individual words
             json = JSON.parse(text);
             console.log('Words loaded!');
+            wordsLoaded();
         })
         .catch(error => {
             console.error('Error fetching words: ', error);
         });
+    randomBackgroundColor();
+}
 
+function randomBackgroundColor() {
+    let random = randInt(135, 225);
+    let colorString = `hsl(${random},80%,90%)`;
+    body.style.backgroundColor = colorString;
 }
-function randomBackgroundColor(){
-    let random = randInt()
 
-    body.style.randomBackgroundColor = colorString;
-}
-function wordsLoaded(){
-    console.log(json);
-    
-}
 // TODO: write function isWord(word)
 
 // For checking word:  json.hasOwnProperty("programming")
 // For array of words: let arr = Object.keys(json)
 // For a random word:  let word = arr[randInt(0, arr.length - 1)];
+const randomWord = document.getElementById("random-word");
+const guessField = document.getElementById("guess-field");
+const feedbackText = document.getElementById("feedback-text");
+let allWords = [];
+let fiveLetterWords = [];
+let secret = '';
+function wordsLoaded() {
+    allWords = Object.keys(json);
+    let randomIndex = randInt(0, allWords.length-1);
+    randomWord.innerHTML = allWords[randomIndex];
+
+    for (let i = 0; i < allWords.length; i++) {
+        let word = allWords[i];
+        if (word.length != 5) continue;
+        fiveLetterWords.push(word);
+    }
+    
+    randomIndex = randInt(0, fiveLetterWords.length-1);
+    secret = fiveLetterWords[randomIndex].toLowerCase();
+}
+
+function changeGuess() {
+    let guess = guessField.value.toLowerCase();
+
+    // SKIP if guess is less than 5 letters
+    if (guess.length < 5) return;
+
+    // SKIP and empty input if guess is more than 5 letters
+    if (guess.length > 5) {
+        guessField.value = "";
+        return;
+    }
+    console.log(`Guess: "${guess}" and Secret: "${secret}"`);
+
+    // SKIP and empty input if guess is NOT a word
+    if (!json.hasOwnProperty(guess)) {
+        feedbackText.innerHTML = `"${guess}" is not a word. Try again.`;
+        guessField.value = "";
+        return;
+    }
+    
+}
